16ffef69d82baf461daa9a0543aa9b3af348f40c
Sergey Kudrin <ksvcsv@inbox.ru>
2025-07-24 16:22:29 +0300

    feat: change ErrorButton component from class to functional with useState

--- src/components/ErrorButton.tsx
+++ src/components/ErrorButton.tsx
@@ -1,34 +1,18 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
-interface State {
-  throwError: boolean;
-}
+export default function ErrorButton() {
+  const [throwError, setThrowError] = useState(false);
 
-export default class ErrorButton extends Component<
-  Record<string, never>,
-  State
-> {
-  state: State = {
-    throwError: false,
-  };
-
-  onClick = () => {
-    this.setState({ throwError: true });
-  };
-
-  render() {
-    if (this.state.throwError) {
-      throw new Error('Test error thrown from ErrorButton!');
-    }
-
-    return (
-      <button
-        onClick={this.onClick}
-        className="fixed bottom-4 right-4 px-4 py-2 bg-red-600 text-white rounded shadow-md
-                   transition duration-300 hover:bg-red-700 hover:scale-105 cursor-pointer"
-      >
-        Error Button
-      </button>
-    );
+  if (throwError) {
+    throw new Error('Test error thrown from ErrorButton!');
   }
+  return (
+    <button
+      onClick={() => setThrowError(true)}
+      className="fixed bottom-4 right-4 px-4 py-2 bg-red-600 text-white rounded shadow-md
+                   transition duration-300 hover:bg-red-700 hover:scale-105 cursor-pointer"
+    >
+      Error Button
+    </button>
+  );
 }
