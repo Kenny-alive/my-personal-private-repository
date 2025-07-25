d0a67d349b90368cf9ef8dae0cb5a569a8027c64
Sergey Kudrin <ksvcsv@inbox.ru>
2025-07-25 13:37:47 +0300

    refactor: remove style test from EmptyState.test

--- src/components/EmptyState.test.tsx
+++ src/components/EmptyState.test.tsx
@@ -11,17 +11,4 @@ describe('EmptyState', () => {
       screen.getByText(/Try a different search term or clear the input/i)
     ).toBeInTheDocument();
   });
-
-  test('has correct styles applied', () => {
-    render(<EmptyState />);
-
-    const title = screen.getByText(/No books found/i);
-    expect(title).toHaveClass('text-6xl');
-    expect(title).toHaveClass('font-extrabold');
-    expect(title).toHaveClass('bg-clip-text');
-    expect(title).toHaveClass('text-transparent');
-
-    const container = title.closest('div');
-    expect(container).toHaveClass('flex', 'items-center', 'justify-center');
-  });
 });
