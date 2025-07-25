1ef1084f9cb2f4bcf03177c1d65ec14b7153caca
Sergey Kudrin <ksvcsv@inbox.ru>
2025-07-25 15:22:43 +0300

    feat: change components ErrorMessage, SearchBar into functional components

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,15 +1,11 @@
-import { Component } from 'react';
-
 interface ErrorMessageProps {
   message: string;
 }
 
-export default class ErrorMessage extends Component<ErrorMessageProps> {
-  render() {
-    return (
-      <div className="text-red-600 text-center min-h-[300px] flex items-center justify-center">
-        <p>Error: {this.props.message}</p>
-      </div>
-    );
-  }
+export default function ErrorMessage({ message }: ErrorMessageProps) {
+  return (
+    <div className="text-red-600 text-center min-h-[300px] flex items-center justify-center">
+      <p>Error: {message}</p>
+    </div>
+  );
 }
