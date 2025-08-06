5a3fd4b34a86ed95f6fbce604ba1359fca04524d
Sergey Kudrin <ksvcsv@inbox.ru>
2025-08-06 15:14:25 +0300

    feat: add TanStack Query with QueryClientProvider and Devtools

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,21 @@ import RouterWrapper from './components/RouterWrapper';
 import './main.css';
 import ErrorBoundary from './components/ErrorBoundary';
 import { ThemeProvider } from './components/ThemeProvider';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { queryClient } from './libs/queryClient';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider>
-      <ErrorBoundary>
-        <BrowserRouter>
-          <RouterWrapper />
-        </BrowserRouter>
-      </ErrorBoundary>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <ErrorBoundary>
+          <BrowserRouter>
+            <RouterWrapper />
+          </BrowserRouter>
+        </ErrorBoundary>
+      </ThemeProvider>
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
   </StrictMode>
 );
