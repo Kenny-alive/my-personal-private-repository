acf8af1f507951718c24ebc0553ca7024fecc5ea
Sergey Kudrin <ksvcsv@inbox.ru>
2025-08-28 16:37:26 +0300

    feat: add basic App component with heading

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">React Performance Task</h1>
+    </div>
+  );
+};
+
+export default App;
